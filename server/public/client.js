ceabafbfcfaa63c5c8e2cd19b2c6be63c1a05d7b
patrickrnelson <patrickrnelson1@gmail.com>
2021-02-11 22:41:59 -0600

    got a response from the client side GET tasks

--- server/public/client.js
+++ server/public/client.js
@@ -4,4 +4,19 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('JQ');
+  getTasks();
+}
+
+function getTasks() {
+  $.ajax({
+    method: 'GET',
+    url: '/tasks',
+  })
+    .then(function (response) {
+      console.log('response', response);
+      renderTasks(response);
+    })
+    .catch(function (error) {
+      console.log('error in GET', error);
+    });
 }

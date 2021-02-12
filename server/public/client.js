b3b01f39099e3da2d5d8bed2ca1b75a85e1ec2ca
patrickrnelson <patrickrnelson1@gmail.com>
2021-02-11 22:48:15 -0600

    Using the GET, SQL data is being appended to DOM

--- server/public/client.js
+++ server/public/client.js
@@ -20,3 +20,17 @@ function getTasks() {
       console.log('error in GET', error);
     });
 }
+
+function renderTasks(array) {
+  for (let task of array) {
+    $('#tableBody').append(`
+      <tr>
+        <td>${task.title}</td>
+        <td>${task.date_created}</td>
+        <td>${task.complete}</td>
+        <td><button>Change</button></td>
+        <td><button>Delete</button></td>
+      </tr>
+      `);
+  }
+}
